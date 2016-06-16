9b7ca3acca61b08c15b421d8dd0f6a74f7a63bdc
Georgi Harizanov <georgi.harizanov@estafet.com>
2016-06-16 18:58:59 +0300

    Added menu

---
+++ states/GameMenu.js
@@ -0,0 +1,53 @@
+var GameMenu = function() {};
+
+
+GameMenu.prototype = {
+
+  menuConfig: {
+    startY: 260,
+    startX: 30
+  },
+
+  init: function () {
+    this.titleText = game.make.text(game.world.centerX, 100, "Lady Gaga", {
+      font: 'bold 60pt TheMinion',
+      fill: '#FDFFB5',
+      align: 'center'
+    });
+    this.subTitleText = game.make.text(game.world.centerX, 160, "And the kitty goblein of power", {
+      font: 'bold 30pt TheMinion',
+      fill: '#FDFFB5',
+      align: 'center'
+    });
+    this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+    this.titleText.anchor.set(0.5);
+    this.subTitleText.anchor.set(0.5);
+    this.optionCount = 1;
+  },
+
+  create: function () {
+
+    if (music.name !== "dangerous" && playMusic) {
+      music.stop();
+      music = game.add.audio('dangerous');
+      music.loop = true;
+      music.play();
+    }
+    game.stage.disableVisibilityChange = true;
+    game.add.sprite(0, 0, 'menu-bg');
+    game.add.existing(this.titleText);
+    game.add.existing(this.subTitleText);
+
+    this.addMenuOption('Start', function () {
+      game.state.start("Game");
+    });
+    this.addMenuOption('Options', function () {
+      game.state.start("Options");
+    });
+    this.addMenuOption('Credits', function () {
+      game.state.start("Credits");
+    });
+  }
+};
+
+Phaser.Utils.mixinPrototype(GameMenu.prototype, mixins);
